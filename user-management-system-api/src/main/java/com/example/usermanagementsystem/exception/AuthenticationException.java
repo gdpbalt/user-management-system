d5ee3af6f00fae5537f3f6d319a6d32a98ea2cd2
gdp <gdp@zigzag.ints.net>
2022-08-31 11:53:14 +0300

    change error code on custom exceptions

--- user-management-system-api/src/main/java/com/example/usermanagementsystem/exception/AuthenticationException.java
+++ user-management-system-api/src/main/java/com/example/usermanagementsystem/exception/AuthenticationException.java
@@ -3,7 +3,7 @@ package com.example.usermanagementsystem.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.CONFLICT)
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
 public class AuthenticationException extends Exception {
     public AuthenticationException(String message) {
         super(message);
